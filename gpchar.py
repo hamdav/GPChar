80285c219acd236c98c0596b4d89139e4b028cff
David Hambraeus <Ddhambraeus@gmail.com>
2023-09-04 10:54:16 +0200

    Threaded approach working

--- gpchar.py
+++ gpchar.py
@@ -1,5 +1,7 @@
 from typing import Callable
-import asyncio
+import threading
+import pdb
+
 
 import numpy as np
 import sklearn.gaussian_process as sgp
@@ -19,8 +21,7 @@ class GPChar:
         bounds: list[tuple],
         n_features: int,
         n_targets: int,
-        previous_evaluation_points: np.ndarray[float] = None,
-        previous_evaluation_values: np.ndarray[float] = None,
+        save_file: str,
         n_initial_samples: int = 20,
         keyword_args: dict = None,
     ):
@@ -50,18 +51,19 @@ class GPChar:
         self.n_features = n_features
         self.n_targets = n_targets
         self.keyword_args = keyword_args if keyword_args is not None else dict()
+        self.gpr_lock = threading.Lock()
+        self.evaulation_lists_lock = threading.Lock()
+        self.lock = threading.Lock()
+        self.save_file = save_file
 
-        if previous_evaluation_values is None and previous_evaluation_points is None:
-            self.evaluation_points = np.empty((0,n_features))
-            self.evaluation_values = np.empty((0,n_targets))
+        self.evaluation_points = np.empty((0,n_features))
+        self.evaluation_values = np.empty((0,n_targets))
 
-        elif ((previous_evaluation_values is None and previous_evaluation_points is not None) or
-              (previous_evaluation_values is not None and previous_evaluation_points is None)):
-            # Error
-            pass
-        else:
-            self.evaluation_points = previous_evaluation_points
-            self.evaluation_values = previous_evaluation_values
+        if save_file is not None:
+            previous_evaluations = np.genfromtxt(save_file, delimiter=",")
+            if previous_evaluations.size > 0:
+                self.evaluation_points = previous_evaluations[:,:n_features]
+                self.evaluation_values = previous_evaluations[:,n_features:]
 
         # TODO: use matern kernel
         # if i use a noise model as well I need some scale on the f
@@ -74,17 +76,47 @@ class GPChar:
 
 
         # Should I normalize y?
-        self.gpr = sgp.GaussianProcessRegressor(kernel=kernel, normalize_y=True)
+        self.gpr = sgp.GaussianProcessRegressor(
+            kernel=kernel,
+            normalize_y=True,
+            n_targets=n_targets
+        )
+        if self.evaluation_points.size > 0:
+            self.gpr.fit(self.evaluation_points, self.evaluation_values)
 
         # TODO: something to say which variance should be used to decide what point(s)
         # are the most uncertian
 
-    async def aquire_random_evaluations(self, n: int):
+    def acquire_random_evaluations_in_thread(self, n: int) -> threading.Thread:
+        thread = threading.Thread(
+            target=self.acquire_random_evaluations,
+            args=(n,)
+        )
+        thread.start()
+        return thread
+
+    def acquire_evaluations_in_thread(self, n: int) -> threading.Thread:
+        thread = threading.Thread(
+            target=self.acquire_new_evaluations,
+            args=(n,)
+        )
+        thread.start()
+        return thread
+    
+
+    def acquire_random_evaluations(self, n: int):
 
         for point in self.get_random_uniform_points(n):
-            new_value = await self.f(point, **self.keyword_args)
+            new_value = self.f(point, **self.keyword_args)
+            self.lock.acquire()
             self.evaluation_points = np.vstack((self.evaluation_points, point))
             self.evaluation_values = np.vstack((self.evaluation_values, new_value))
+            with open(self.save_file, "ab") as f:
+                np.savetxt(f, [np.concatenate((point,new_value))],
+                           delimiter=",")
+
+            self.gpr.fit(self.evaluation_points, self.evaluation_values)
+            self.lock.release()
 
     def get_random_uniform_points(self, n: int) -> np.ndarray[float]:
         """
@@ -103,18 +135,31 @@ class GPChar:
         prediction, std = self.gpr.predict(x, return_std=True)
         return std
 
-    async def acquire_new_evaluation(self):
+    def acquire_new_evaluations(self, n: int):
         """
         finds new point to evaluate function at in order to maximize gained information
         and evaluates function there, adding the data to the evaluation_points and 
         evaluation_values arrays
         """
-        # Minimize acquisition function (variance?)
-        res = sciopt.minimize(self.acquisition_function, self.get_random_uniform_points(1)[0], bounds=self.bounds)
-        new_value = await self.f(res.x, **self.keyword_args)
-        self.evaluation_points = np.vstack((self.evaluation_points, res.x))
-        self.evaluation_values = np.vstack((self.evaluation_values, new_value))
-        self.gpr.fit(self.evaluation_points, self.evaluation_values)
+        for _ in range(n):
+            # Minimize acquisition function (variance?)
+            self.lock.acquire()
+            res = sciopt.minimize(self.acquisition_function, self.get_random_uniform_points(1)[0], bounds=self.bounds)
+            self.lock.release()
+
+            # Calculate new value
+            new_value = self.f(res.x, **self.keyword_args)
+
+            # Add value and evaluation point to lists
+            self.lock.acquire()
+            self.evaluation_points = np.vstack((self.evaluation_points, res.x))
+            self.evaluation_values = np.vstack((self.evaluation_values, new_value))
+            with open(self.save_file, "ab") as f:
+                np.savetxt(f, [np.concatenate((res.x,new_value))],
+                           delimiter=",")
+
+            self.gpr.fit(self.evaluation_points, self.evaluation_values)
+            self.lock.release()
 
     def get_1d_prediction(self, dimension: int, point: np.ndarray[float]) -> np.ndarray[float]:
         """
@@ -135,7 +180,9 @@ class GPChar:
         points = np.tile(point, (100,1))
         points[:,dimension] = xs
 
+        self.lock.acquire()
         ys, stds = self.gpr.predict(points, return_std=True)
+        self.lock.release()
         return xs, ys, stds
 
     def get_2d_prediction(self, dimension1: int, dimension2: int, point: np.ndarray[float]) -> np.ndarray[float]:
@@ -161,8 +208,10 @@ class GPChar:
         x1s = np.linspace(*self.bounds[dimension1], n1)
         x2s = np.linspace(*self.bounds[dimension2], n2)
         points = np.tile(point, (n1*n2,1))
-        points[:,dimension1] = np.repeat(x1s, n2)
-        points[:,dimension2] = np.tile(x1s, n1)
+        points[:,dimension1] = np.tile(x1s, n2)
+        points[:,dimension2] = np.repeat(x2s, n1)
 
+        self.lock.acquire()
         ys, stds = self.gpr.predict(points, return_std=True)
+        self.lock.release()
         return x1s, x2s, ys, stds
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from gpchar import GPChar
 import pdb
-import asyncio
+import time
+import threading
 
 from dash import Dash, dcc, html, Input, Output, callback, ALL
 import plotly.graph_objects as go
@@ -8,9 +9,9 @@ import plotly.graph_objects as go
 import numpy as np
 import matplotlib.pyplot as plt
 
-async def f(x, a, b):
+def f(x, a, b):
     print("Running f", x)
-    await asyncio.sleep(1)
+    time.sleep(10)
     m = np.array([
         [np.cos(a)*np.cos(b), np.sin(a)*np.cos(b), np.cos(b)],
         [-np.sin(a)*np.cos(b), np.sin(a)*np.cos(b), -np.cos(b)]])
@@ -87,6 +88,7 @@ def dash_version():
                 y=ys[:,outputs.index(y_dim)],
                 mode='lines',
                 line=dict(color='rgb(31, 119, 180)'),
+                showlegend=False
             ),
             go.Scatter(
                 name='Upper Bound',
@@ -116,12 +118,12 @@ def dash_version():
 
     app.run(debug=True)
 
-async def dash_2d_version():
+def dash_2d_version():
     bounds = [(0,1), (2,4), (-np.pi, np.pi)]
     inputs = ["x0", "x1", "x2"]
     outputs = ["y0", "y1"]
-    gpc = GPChar(f, bounds, 3, 2, keyword_args={"a": 1, "b": 2})
-    await gpc.aquire_random_evaluations(20)
+    gpc = GPChar(f, bounds, 3, 2, "data.csv", keyword_args={"a": 1, "b": 2})
+    gpc.acquire_random_evaluations_in_thread(3)
 
     sliders = [
         dcc.Slider(
@@ -161,62 +163,106 @@ async def dash_2d_version():
                 style={'width': '20%', 'display': 'inline-block'},
             )
         ]),
+        dcc.Graph(id='1d-graph',
+                  style={'width': '33%', 'display': 'inline-block'}),
         dcc.Graph(id='mean-contour',
-                  style={'width': '49%', 'display': 'inline-block'}),
+                  style={'width': '33%', 'display': 'inline-block'}),
         dcc.Graph(id='std-contour',
-                  style={'width': '49%', 'display': 'inline-block'}),
+                  style={'width': '33%', 'display': 'inline-block'}),
     ] + sliders
     )
 
 
     @app.callback(
+        Output('1d-graph', 'figure'),
         Output('mean-contour', 'figure'),
-        Input('input1-dropdown', 'value'),
-        Input('input2-dropdown', 'value'),
-        Input('output-dropdown', 'value'),
-        Input({"type": "slider", "index": ALL}, "value")
-    )
-    def update_mean_figure(x1_dim, x2_dim, y_dim, values):
-
-        print("Predicting...")
-        x1s, x2s, ys, stds = gpc.get_2d_prediction(inputs.index(x1_dim), inputs.index(x2_dim), np.array(values))
-        print("Done")
-        fig = go.Figure([
-            go.Contour(
-                name='Mean',
-                x=x1s,
-                y=x2s,
-                z=np.reshape(ys[:,outputs.index(y_dim)], (100,100)),
-                colorbar=dict(
-                    title=y_dim,
-                    titleside='right',
-                    titlefont=dict(
-                        size=14,
-                        family='Arial, sans-serif')
-                )
-            ),
-        ])
-
-        fig.update_layout(transition_duration=100)
-
-        return fig
-
-    @app.callback(
         Output('std-contour', 'figure'),
         Input('input1-dropdown', 'value'),
         Input('input2-dropdown', 'value'),
         Input('output-dropdown', 'value'),
         Input({"type": "slider", "index": ALL}, "value")
     )
-    def update_std_figure(x1_dim, x2_dim, y_dim, values):
+    def update_figures(x1_dim, x2_dim, y_dim, values):
+        #pdb.set_trace()
+        #breakpoint()
+        xs, ys, stds = gpc.get_1d_prediction(inputs.index(x1_dim), np.array(values))
+        oned_fig = go.Figure([
+            go.Scatter(
+                name='Mean',
+                x=xs,
+                y=ys[:,outputs.index(y_dim)],
+                mode='lines',
+                line=dict(color='rgb(31, 119, 180)'),
+                showlegend=False
+            ),
+            go.Scatter(
+                name='Upper Bound',
+                x=xs,
+                y=ys[:,outputs.index(y_dim)] + stds[:,outputs.index(y_dim)],
+                mode='lines',
+                marker=dict(color="#444"),
+                line=dict(width=0),
+                showlegend=False
+            ),
+            go.Scatter(
+                name='Lower Bound',
+                x=xs,
+                y=ys[:,outputs.index(y_dim)] - stds[:,outputs.index(y_dim)],
+                marker=dict(color="#444"),
+                line=dict(width=0),
+                mode='lines',
+                fillcolor='rgba(68, 68, 68, 0.3)',
+                fill='tonexty',
+                showlegend=False
+            )
+        ])
+
+        oned_fig.update_layout(
+            transition_duration=100,
+            xaxis_title=x1_dim,
+            yaxis_title=y_dim
+        )
+
 
         x1s, x2s, ys, stds = gpc.get_2d_prediction(inputs.index(x1_dim), inputs.index(x2_dim), np.array(values))
-        fig = go.Figure([
+        mean_zs = np.reshape(ys[:,outputs.index(y_dim)], (100,100))
+        std_zs = np.reshape(stds[:,outputs.index(y_dim)], (100,100))
+
+        mean_fig = go.Figure([
             go.Contour(
                 name='Mean',
                 x=x1s,
                 y=x2s,
-                z=np.reshape(stds[:,outputs.index(y_dim)], (100,100)),
+                z=mean_zs,
+                colorbar=dict(
+                    title=y_dim,
+                    titleside='right',
+                    titlefont=dict(
+                        size=14,
+                        family='Arial, sans-serif')
+                ),
+                line=dict(width=0),
+                ncontours=20,
+            ),
+            go.Scatter(
+                name='1d-marker-line',
+                x=x1s,
+                y=np.full(x1s.shape, values[inputs.index(x2_dim)]),
+            )
+        ])
+
+        mean_fig.update_layout(
+            transition_duration=100,
+            xaxis_title=x1_dim,
+            yaxis_title=x2_dim
+        )
+
+        std_fig = go.Figure([
+            go.Contour(
+                name='Uncertainty',
+                x=x1s,
+                y=x2s,
+                z=std_zs,
                 colorbar=dict(
                     title=y_dim + " uncertainty",
                     titleside='right',
@@ -225,15 +271,28 @@ async def dash_2d_version():
                         family='Arial, sans-serif')
                 ),
                 colorscale='Reds',
+                line=dict(width=0),
+                zmin=0,
+                zmax=np.max(std_zs),
+                ncontours=20,
             ),
+            go.Scatter(
+                name='1d-marker-line',
+                x=x1s,
+                y=np.full(x1s.shape, values[inputs.index(x2_dim)]),
+            )
         ])
 
-        fig.update_layout(transition_duration=100)
+        std_fig.update_layout(
+            transition_duration=100,
+            xaxis_title=x1_dim,
+            yaxis_title=x2_dim
+        )
 
-        return fig
+        return oned_fig, mean_fig, std_fig
 
-    app.run(debug=True)
+    app.run(debug=True,use_reloader=False)
 
 if __name__ == '__main__':
-    asyncio.run(dash_2d_version())
+    dash_2d_version()
     #dash_version()
